04bd02154ec21d6fec693900342d68c66e0b04c7
pkinyua <pkinyua@calwide.co.ke>
2026-02-15 23:55:32 +0300

    fix allowed hosts

--- teachers_portal/settings.py
+++ teachers_portal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lj5b&@=@hl*b06a*7os2hfrm#8a5fjgw*01fmzy&oit69eb^g^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
